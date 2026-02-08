5c9960e52b3de6042783c99a2dc93eda942e334b
Yasha AI <yasha.friend.55@gmail.com>
2026-02-08 10:15:28 +0100

    feat: migrate to Nextra static site

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default {
+  logo: <span>Yasha Learn Code</span>,
+  project: {
+    link: 'https://github.com/yasha-ai/yasha-learn-code'
+  },
+  docsRepositoryBase: 'https://github.com/yasha-ai/yasha-learn-code/tree/main',
+  footer: {
+    text: 'Создано с 💜 от Яши'
+  },
+  primaryHue: 142, // Green
+  darkMode: true,
+  nextThemes: {
+    defaultTheme: 'dark'
+  }
+}
