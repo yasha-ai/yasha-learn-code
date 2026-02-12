e139e998e8ccff3d8019ccddcdeab66aa4e3499b
Yasha <yasha.friend.55@gmail.com>
2026-02-12 09:36:17 +0100

    chore(build): optimize build speed (disable git timestamps, skip lint/ts check)

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx'
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone', // Оптимизация для Docker
+  eslint: {
+    // Внимание: Это отключает проверку линтером во время продакшн-сборки
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Внимание: Это отключает проверку типов во время продакшн-сборки
+    ignoreBuildErrors: true,
+  },
+})
